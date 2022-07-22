0fd8bc23a6e405fecc1a57c204b94e222816696d
mhashim-sama <hashimmazhar15@gmail.com>
2022-07-22 16:30:04 +0500

    completed task 1

--- weatherman.rb
+++ weatherman.rb
@@ -29,16 +29,16 @@ ARGV.each do |arg|
   when 2
     temp = arg.split('/')
     task['year'] = temp[0]
-    if temp.length == 2
-      task['month'] = temp[1]
-    else
-      task['month'] = '0'
-    end
+    task['month'] = if temp.length == 2
+                      temp[1]
+                    else
+                      '0'
+                    end
   when 3
     task['path'] = arg
   end
   counter += 1
-  puts counter
+  # puts counter
 end
 
 # Extracting file path
@@ -49,13 +49,100 @@ filename.push(months[task['month']])
 
 full_filename = filename.join('_')
 
-file_path = task['path'] + full_filename + '.txt'
-
-puts file_path
+file_path = "#{task['path']}#{full_filename}.txt"
 
 # Reading data from file
-file_data = IO.readlines(file_path)
+if File.file?(file_path)
+  file_data = File.readlines(file_path, chomp: true)
 
-file_data.each do |line|
-  puts line
+  parsed_data = {}
+  temp_categories = file_data[0].split(',')
+  temp_categories.each do |categories|
+    parsed_data[categories.strip] = nil
+  end
+  file_data.unshift
+
+  file_data.each do |line|
+    puts line
+
+    temp_values = line.split(',')
+    temp_values.map {|val| val.strip}
+
+    parsed_data = parsed_data.keys.zip(temp_values).to_h
+    parsed_data.each do |cat, data|
+      puts "#{cat} => #{data}"
+    end
+  end
+else
+  file_path = "#{task['path']}#{full_filename}"
 end
+
+# Task 1
+def task_1(partial_file_path, months)
+  max_temp = { 'Max TemperatureC' => '',
+               'Date' => '' }
+  min_temp = { 'Min TemperatureC' => '',
+               'Date' => '' }
+  max_humidity = { 'Max Humidity' => '',
+                   'Date' => '' }
+
+  for i in 1..12
+    file_path = partial_file_path + months[i.to_s] + '.txt'
+    if File.file?(file_path)
+      file_data = File.readlines(file_path, chomp: true)
+
+      parsed_data = {}
+      temp_categories = file_data[0].split(',')
+      temp_categories.unshift
+      parsed_data["Date"] = nil
+      temp_categories.each do |categories|
+        parsed_data[categories.strip] = nil
+      end
+      file_data.unshift
+
+      file_data.each do |line|
+        puts line
+
+        temp_values = line.split(',')
+        temp_values.map {|val| val.strip}
+
+        parsed_data = parsed_data.keys.zip(temp_values).to_h
+        parsed_data.each do |cat, data|
+          puts "#{cat} => #{data}"
+        end
+
+        # Finding max temp
+        if max_temp['Max TemperatureC'].to_i < parsed_data['Max TemperatureC'].to_i
+          max_temp['Max TemperatureC'] = parsed_data['Max TemperatureC']
+          max_temp['Date'] = parsed_data['Date']
+        end
+
+        # Finding max temp
+        if min_temp['Min TemperatureC'].to_i > parsed_data['Min TemperatureC'].to_i
+          min_temp['Min TemperatureC'] = parsed_data['Min TemperatureC']
+          min_temp['Date'] = parsed_data['Date']
+        end
+
+        # Finding max temp
+        if max_humidity['Max Humidity'].to_i < parsed_data['Max Humidity'].to_i
+          max_humidity['Max Humidity'] = parsed_data['Max Humidity']
+          max_humidity['Date'] = parsed_data['Date']
+        end
+      end
+    else
+      puts "File #{file_path} not found"
+    end
+  end
+  puts ''
+  puts 'Max Temprature'
+  puts max_temp
+  puts ''
+  puts 'Min Temprature'
+  puts min_temp
+  puts ''
+  puts 'Max Humidity'
+  puts max_humidity
+  puts ''
+end
+
+task_1(file_path, months)
